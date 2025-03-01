5d628ee9f3116d599ca17c579f6725915d34ec9e
wok <wok@example.com>
2025-03-02 01:36:44 +0900

    update

--- GPT_SoVITS/inference_webui.py
+++ GPT_SoVITS/inference_webui.py
@@ -845,40 +845,40 @@ def process_text(texts):
     return _text
 
 
-# def change_choices():
-#     SoVITS_names, GPT_names = get_weights_names(GPT_weight_root, SoVITS_weight_root)
-#     return {"choices": sorted(SoVITS_names, key=custom_sort_key), "__type__": "update"}, {"choices": sorted(GPT_names, key=custom_sort_key), "__type__": "update"}
+def change_choices():
+    SoVITS_names, GPT_names = get_weights_names(GPT_weight_root, SoVITS_weight_root)
+    return {"choices": sorted(SoVITS_names, key=custom_sort_key), "__type__": "update"}, {"choices": sorted(GPT_names, key=custom_sort_key), "__type__": "update"}
 
 
-# SoVITS_weight_root=["SoVITS_weights","SoVITS_weights_v2","SoVITS_weights_v3"]
-# GPT_weight_root=["GPT_weights","GPT_weights_v2","GPT_weights_v3"]
-# for path in SoVITS_weight_root+GPT_weight_root:
-#     os.makedirs(path,exist_ok=True)
+SoVITS_weight_root=["SoVITS_weights","SoVITS_weights_v2","SoVITS_weights_v3"]
+GPT_weight_root=["GPT_weights","GPT_weights_v2","GPT_weights_v3"]
+for path in SoVITS_weight_root+GPT_weight_root:
+    os.makedirs(path,exist_ok=True)
 
 
-# def get_weights_names(GPT_weight_root, SoVITS_weight_root):
-#     SoVITS_names = [i for i in pretrained_sovits_name]
-#     for path in SoVITS_weight_root:
-#         for name in os.listdir(path):
-#             if name.endswith(".pth"): SoVITS_names.append("%s/%s" % (path, name))
-#     GPT_names = [i for i in pretrained_gpt_name]
-#     for path in GPT_weight_root:
-#         for name in os.listdir(path):
-#             if name.endswith(".ckpt"): GPT_names.append("%s/%s" % (path, name))
-#     return SoVITS_names, GPT_names
+def get_weights_names(GPT_weight_root, SoVITS_weight_root):
+    SoVITS_names = [i for i in pretrained_sovits_name]
+    for path in SoVITS_weight_root:
+        for name in os.listdir(path):
+            if name.endswith(".pth"): SoVITS_names.append("%s/%s" % (path, name))
+    GPT_names = [i for i in pretrained_gpt_name]
+    for path in GPT_weight_root:
+        for name in os.listdir(path):
+            if name.endswith(".ckpt"): GPT_names.append("%s/%s" % (path, name))
+    return SoVITS_names, GPT_names
 
 
-# SoVITS_names, GPT_names = get_weights_names(GPT_weight_root, SoVITS_weight_root)
+SoVITS_names, GPT_names = get_weights_names(GPT_weight_root, SoVITS_weight_root)
 
-# def html_center(text, label='p'):
-#     return f"""<div style="text-align: center; margin: 100; padding: 50;">
-#                 <{label} style="margin: 0; padding: 0;">{text}</{label}>
-#                 </div>"""
+def html_center(text, label='p'):
+    return f"""<div style="text-align: center; margin: 100; padding: 50;">
+                <{label} style="margin: 0; padding: 0;">{text}</{label}>
+                </div>"""
 
-# def html_left(text, label='p'):
-#     return f"""<div style="text-align: left; margin: 0; padding: 0;">
-#                 <{label} style="margin: 0; padding: 0;">{text}</{label}>
-#                 </div>"""
+def html_left(text, label='p'):
+    return f"""<div style="text-align: left; margin: 0; padding: 0;">
+                <{label} style="margin: 0; padding: 0;">{text}</{label}>
+                </div>"""
 
 
 with gr.Blocks(title="GPT-SoVITS WebUI") as app:
